30b30ae3afbc11da417a2422a62481262810cc95
PolyFran <po.fran@hotmail.com>
2022-03-31 04:03:17 -0300

    Implementação Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -5,29 +5,26 @@ import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
 import './index.css';
 
 function App() {
   return (
     <Router>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="Contact">Contact</Link>
-          <Link to="Company">Company</Link>
-          <Link to="NewProject">NewProject</Link>
-          <li>Contato</li>
-        </div>
-
-            <Container customClass= "min-height">
-              <Routes>
-                <Route exact path='/' element = {<><Home /></>}></Route>
-                <Route exact path='company' element = {<><Company /></>}></Route>
-                <Route exact path='contact' element = {<><Contact /></>}></Route>
-                <Route exact path='newproject' element = {<><NewProject /></>}></Route>
-              </Routes>
-            </Container>
+      <Navbar />
+        <Container customClass= "min-height">
+          <Routes>
+            <Route exact path='/' element = {<><Home /></>}></Route>
+            <Route exact path='projects' element = {<><Projects /></>}></Route>
+            <Route exact path='company' element = {<><Company /></>}></Route>
+            <Route exact path='contact' element = {<><Contact /></>}></Route>
+            <Route exact path='newproject' element = {<><NewProject /></>}></Route>
+          </Routes>
+        </Container>
       
-        <p>Footer</p>
+        <Footer />
     </Router>
   
   );
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom'
+import Container from './Container'
+import styles from './Navbar.module.css'
+import logo from '../../img/costs_logo.png'
+
+function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to="/">
+                    <img src={logo} alt="Costs"/>
+                </Link>
+                <ul class={styles.list}>
+                    <li className={styles.item}> <Link to="Contact">Contact</Link></li>
+                    <li className={styles.item}><Link to="/">Home</Link></li>
+                    <li className={styles.item}><Link to="Company">Company</Link></li>
+                    <li className={styles.item}> <Link to="Projects">Projects</Link></li>
+                </ul>
+            </Container>
+      </nav>
+    )
+}
+
+export default Navbar
