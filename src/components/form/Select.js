d722209dbda0215e683c11b63546a20b781c461a
PolyFran <po.fran@hotmail.com>
2022-04-01 09:51:41 -0300

    Inserido dados no BD via API

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,7 +6,10 @@ function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}:</label>
             <select
                 name={name}
-                id={name}>
+                id={name}
+                onChange={handleOnChange}
+                value={value || ''}
+                >
                 <option>Selecione a opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
