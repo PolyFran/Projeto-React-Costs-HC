2b25c2af33961078d881740c5442d2762b8a5711
PolyFran <po.fran@hotmail.com>
2022-04-04 12:42:01 -0300

    alguns ajustes no contato

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -17,12 +17,12 @@ function Company() {
                 Sempre atentos às novas tecnologias, possuímos uma equipe altamente especializada, formada por profissionais em constante 
                 processo de busca de novos conhecimentos, de aprendizado e de atualização em áreas de gestão de informações. 
                 Além de uma sólida formação nas áreas de Tecnologia da Informação, temos uma vasta experiência prática na geração de 
-                instrumentos de apoio para médias e grandes empresas. Situados na cidade de Ijuí/RS, dispomos da capacidade para atender 
+                instrumentos de apoio para médias e grandes empresas. Situados na cidade de Criciúma/RS, dispomos da capacidade para atender 
                 todo o estado. Também firmamos parceria com profissionais de outras organizações e áreas para garantir que cada projeto 
                 entregue beneficie ao máximo nossos clientes.</p>
 
                 
-                <h1> Tecnologias</h1>
+                <h1> Principais Tecnologias</h1>
 
                 <div>
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,25 @@
+import styles from './Company.module.css'
+
 function Contact() {
-    return <h1>Contato</h1>
+    return (
+
+    <div>
+        <h1>Contato</h1>
+    
+
+        <div className={styles.company_container}>
+
+            <h2>Rua Pietro Benneton, 542, apto 701 </h2>
+            <h2>Centro - Criciúma </h2>
+            <h2>CEP: 88804-070</h2>
+            <h2>Telefone: (48) XXXXX XXXX</h2>
+            <h2>E-mail: pereira.tec@gmail.com</h2>
+
+        
+
+        </div>
+        </div>
+    )
 }
 
 export default Contact
